c8dbf408e0acc3ff874fe58ffd824bba805e8302
Hassan Sherif Hassan Elkersh <122812098+Hassannetsha@users.noreply.github.com>
2026-04-24 15:06:02 +0300

    Add: minimum-window-with-characters - submission-2

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-2.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        if(t.size()>s.size()){
+            return "";
+        }
+        map<int,deque<char>>mp;
+        unordered_map<char,int>cnt;
+        for(int i = 0;i<t.size();i++){
+            cnt[t[i]]++;
+        }
+        int l = 0,r = 0,cntCh = 0,mnSize = s.size()+1;
+        // unordered_set<char>st(t.begin(),t.end());
+        deque<char> temp;
+        while(r<s.size()){
+            while(cntCh<cnt.size()&&r<s.size()){
+                if(cnt.find(s[r])!=cnt.end()){
+                    cnt[s[r]]--;
+                    if(cnt[s[r]]==0){
+                        cntCh++;
+                    }
+                }
+                temp.push_back(s[r]);
+                r++;
+            }
+            if(cntCh==cnt.size()){
+                mnSize = min(mnSize,r-l);
+                mp[r-l] = temp;
+            }
+            while(cntCh==cnt.size()){
+                if(cnt.find(s[l])!=cnt.end()){
+                    cnt[s[l]]++;
+                    if(cnt[s[l]]==1){
+                        cntCh--;
+                    }
+                }
+                temp.pop_front();
+                if(cntCh==cnt.size()){
+                    mnSize = min(mnSize,r-l);
+                    mp[r-l] = temp;
+                }
+                l++;
+            }
+        }
+        if(mnSize==s.size()+1)
+        {
+            return "";
+        }
+        string ans;
+        temp = mp[mnSize];
+        for(auto& it:temp){
+            ans.push_back(it);
+        }
+        return ans;
+    }
+};
