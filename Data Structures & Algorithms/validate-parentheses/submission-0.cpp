b40f08c78dbc660f9d3ebe1d239dcfd6327e6570
Hassan Sherif Hassan Elkersh <122812098+Hassannetsha@users.noreply.github.com>
2026-04-25 18:40:09 +0300

    Add: validate-parentheses - submission-0

---
+++ Data Structures & Algorithms/validate-parentheses/submission-0.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char>st;
+        for(auto& it:s){
+            if(it=='('||it=='['||it=='{'){
+                st.push(it);
+            }
+            else if (!st.empty()){
+                if(st.top()=='('&&it==')'||st.top()=='{'&&it=='}'||st.top()=='['&&it==']'){
+                    st.pop();
+                }
+                else{
+                    return false;
+                }
+            }
+            else{
+                return false;
+            }
+        }
+        return st.empty();
+    }
+};
