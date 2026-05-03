512d2e61c86d93a9d08cc741c6ced64d60c41300
Hassan Sherif Hassan Elkersh <122812098+Hassannetsha@users.noreply.github.com>
2026-05-03 22:58:42 +0300

    Add: time-based-key-value-store - submission-1

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-1.cpp
@@ -0,0 +1,22 @@
+class TimeMap {
+public:
+    unordered_map<string,map<int,string>>mp;
+    TimeMap() {
+        
+    }
+    
+    void set(string key, string value, int timestamp) {
+        mp[key].insert({timestamp,value});
+    }
+    
+    string get(string key, int timestamp) {
+        map<int,string>search = mp[key];
+        auto it = search.upper_bound(timestamp);
+        if(it==search.begin()){
+            return "";
+        }
+        it--;
+        
+        return it->second;
+    }
+};
