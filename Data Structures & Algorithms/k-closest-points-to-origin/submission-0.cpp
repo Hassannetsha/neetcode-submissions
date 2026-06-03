444344e7ba1556454d3b2c3efef88aac23de0d5f
Hassan Sherif Hassan Elkersh <122812098+Hassannetsha@users.noreply.github.com>
2026-06-04 00:56:46 +0300

    Add: k-closest-points-to-origin - submission-0

---
+++ Data Structures & Algorithms/k-closest-points-to-origin/submission-0.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        vector<vector<int>>ans;
+        priority_queue<pair<double,vector<int>>,vector<pair<double,vector<int>>>,greater<pair<double,vector<int>>>>pq;
+        for(int i = 0;i<points.size();i++){
+            double dis = sqrt(pow(points[i][0],2) + pow(points[i][1],2));
+            pq.emplace(dis,points[i]);
+        }
+        for(int i = 0;i<k;i++){
+            ans.push_back(pq.top().second);
+            pq.pop();
+        }
+        return ans;
+    }
+};
