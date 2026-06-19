73545e69fb8d390494114006e0160388f391a008
Hassan Sherif Hassan Elkersh <122812098+Hassannetsha@users.noreply.github.com>
2026-06-19 14:02:36 +0300

    Add: subsets - submission-0

---
+++ Data Structures & Algorithms/subsets/submission-0.cpp
@@ -0,0 +1,20 @@
+class Solution {
+    bool knowBit(int& num,int& bit){
+        return (num>>bit)&1;
+    }
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>>ans;
+        for(int i = 0;i<(1<<nums.size());i++){
+            vector<int>temp;
+            for(int j = 0;j<nums.size();j++){
+                if(knowBit(i,j)){
+                    temp.push_back(nums[j]);
+                }
+            }
+            ans.push_back(temp);
+        }
+        sort(ans.begin(),ans.end());
+        return ans;
+    }
+};
